4af088b81ec2d65ea94007d0f31e66a249474104
Xavier R (eXsoR) <xrodriguez65@gmail.com>
2021-07-19 13:48:41 -0400

    My custom Keymap for Keychron K6

---
+++ k6/led_matrix.c
@@ -0,0 +1,94 @@
+#include <string.h>
+#include "rgb.h"
+#include "rgb_matrix.h"
+#include "rgb_matrix_types.h"
+#include "color.h"
+
+/*
+    COLS key / led
+    PWM PWM08A - PWM02B
+    Y2 transistors PNP driven high
+    base      - GPIO
+    collector - LED Col pins
+    emitter   - VDD
+
+    VDD     GPIO
+    (E)     (B)
+     |  PNP  |
+     |_______|
+         |
+         |
+        (C)
+        LED
+
+    ROWS RGB
+    PWM PWM05B - PWM22B (PWM17B unused)
+    C 0-15
+    Y1 transistors NPN driven low
+    base      - GPIO
+    collector - LED RGB row pins
+    emitter   - GND
+
+        LED
+        (C)
+         |
+         |
+      _______
+     |  NPN  |
+     |       |
+    (B)     (E)
+    GPIO    GND
+*/
+
+LED_TYPE led_state[LED_MATRIX_ROWS * LED_MATRIX_COLS];
+LED_TYPE new_led_state[LED_MATRIX_ROWS * LED_MATRIX_COLS];
+uint8_t led_pos[DRIVER_LED_TOTAL];
+
+void init(void) {
+    unsigned int i = 0;
+    for (unsigned int y = 0; y < LED_MATRIX_ROWS; y++) {
+        for (unsigned int x = 0; x < LED_MATRIX_COLS; x++) {
+            if (g_led_config.matrix_co[y][x] != NO_LED) {
+                led_pos[g_led_config.matrix_co[y][x]] = i;
+            }
+            i++;
+        }
+    }
+}
+
+static void flush(void) {
+    for (int i = 0; i < LED_MATRIX_ROWS * LED_MATRIX_COLS; i++)
+        led_state[i] = new_led_state[i];
+}
+
+void set_color(int index, uint8_t r, uint8_t g, uint8_t b) {
+    int corrected_index = led_pos[index];
+    new_led_state[corrected_index].r = r;
+    new_led_state[corrected_index].g = g;
+    new_led_state[corrected_index].b = b;
+}
+
+static void set_color_all(uint8_t r, uint8_t g, uint8_t b) {
+    for (int i=0; i<DRIVER_LED_TOTAL; i++)
+        set_color(i, r, g, b);
+}
+
+const rgb_matrix_driver_t rgb_matrix_driver = {
+    .init          = init,
+    .flush         = flush,
+    .set_color     = set_color,
+    .set_color_all = set_color_all,
+};
+/*
+// byte order: R,B,G
+static uint8_t caps_lock_color[3] = { 0x00, 0x00, 0xFF };
+
+void led_set(uint8_t usb_led) {
+    // dk63 has only CAPS indicator
+    if (usb_led >> USB_LED_CAPS_LOCK & 1) {
+        set_color(11, caps_lock_color[0], caps_lock_color[2], caps_lock_color[1]);
+    } else {
+        set_color(11, 0x00, 0x00, 0x00);
+    }
+}
+*/
